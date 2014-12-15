2dbb7217b77b8b510f70814048818a14a2eca8ec
Christopher Franks <christopher.franks@adelaide.edu.au>
2014-12-15 13:58:32 +1030

    Revised ACAD models per test metadata

--- acad_models.py
+++ acad_models.py
@@ -1,5 +1,5 @@
-# Django models based on MODC08 Metadata Schema v1.0
-# CF 21/11/14, 27/11/14
+# Django models based on MODC08 Metadata Schema v1.1
+# CF 21/11/14, 27/11/14, 15/12/14
 
 from django.db import models
 
@@ -51,22 +51,24 @@ class Source(models.Model):
     other_id_type = models.CharField("Who assigned other id", max_length=255, blank=True)
     date = models.DateField("Date source was collected")
     organism = models.ForeignKey(Organism)
-    source_details = models.CharField("Details of sample i.e. what it is", max_length=255)
+    source_details = models.CharField("Details of sample", max_length=255)
     gender = models.CharField("Gender", choices=GENDERS, max_length=255)
     age_cat = models.CharField("Age category", choices=AGE_CATS, max_length=255)
     age_range = models.CharField("Estimated age range of source", max_length=255, blank=True)
     geoloc_continent = models.CharField("Continent", choices=CONTINENTS, max_length=255)
     geoloc_country = models.CharField("Country or sea", max_length=255, blank=True)
     geoloc_locale = models.CharField("Town, city or other locality", max_length=255, blank=True)
-    geoloc_lat = models.FloatField()
-    geoloc_lon = models.FloatField()
+    geoloc_lat = models.FloatField(blank=True, null=True)
+    geoloc_lon = models.FloatField(blank=True, null=True)
     env_biome = models.CharField("Broad ecological context of where source collected", max_length=255, blank=True)
     env_feature = models.CharField("Geographic environmental features", max_length=255, blank=True)
     env_material = models.CharField("Material in which source was embedded, or material displaced", max_length=255, blank=True)
     carbondate_years = models.PositiveSmallIntegerField("Estimated age of source in radiocarbon years", blank=True, null=True)
-    carbondate_error = models.PositiveSmallIntegerField("Estimated carbon date error range (in radiocarbon years)", blank=True, null=True)
+    carbondate_error = models.PositiveSmallIntegerField("Estimated carbon date error range", blank=True, null=True)
     carbondate_id = models.CharField("Centre + id reference for carbon dating", blank=True, max_length=255)
     source_notes = models.TextField("Free text notes about source", blank=True)
+    group_id = models.CharField("Group/set of sources collected on one trip/survey", max_length=255, blank=True)
+    collectedby = models.CharField("Individual/team who collected source", max_length=255, blank=True)
 
     def __unicode__(self):
         return str(self.organism.get_common_name()) + " " + self.source_details + ", " + self.geoloc_country + " " + str(self.date)
@@ -76,6 +78,10 @@ class Source(models.Model):
 
     def get_source_id(self):
         return self.id
+
+    def get_absolute_url(self):
+        from django.core.urlresolvers import reverse
+        return reverse('source_detail', args=[str(self.id)])
 
 class Sample(models.Model):
     SAMPLE_CATS = (
@@ -91,22 +97,34 @@ class Sample(models.Model):
     )
 
     id = models.CharField("Sample id", primary_key=True, max_length=255)
+    id_type = models.CharField("Who assigned source id", max_length=255)
     source = models.ForeignKey(Source)
     date = models.DateField("Date sample was collected")
+    organism = models.ForeignKey(Organism)
     sample_cat = models.CharField("Category of sample", choices=SAMPLE_CATS, max_length=255)
     sample_details = models.CharField("Sample details/description", max_length=255)
     env_package = models.CharField("Environmental package/category of sample", max_length=255, choices=ENV_PACKAGES, blank=True)
+    group_id = models.CharField("Group/set of sources collected on one trip/survey", max_length=255, blank=True)
+    acad_loc = models.CharField("Where physically held in ACAD", max_length=255, blank=True)
+    collectedby = models.CharField("Individual/team who collected source", max_length=255, blank=True)
     sample_notes = models.TextField("Free text notes about sample", blank=True)
 
     def __unicode__(self):
-        return self.get_sample_cat_display() + " (" + self.sample_details + ") from " + " source " + str(self.source.get_source_id())
+        return self.get_sample_cat_display() + " (" + self.sample_details + ") " + self.id
+
+    def get_id(self):
+        return self.id
+
+    def get_absolute_url(self):
+        from django.core.urlresolvers import reverse
+        return reverse('sample_detail', args=[str(self.id)])
 
 class Extract(models.Model):
     id = models.CharField("Extract ID", primary_key=True, max_length=255)
     sample = models.ForeignKey(Sample)
     date = models.DateField("Date extract was done")
     protocol_ref = models.CharField("Publication describing library construction method", max_length=255)
-    protocol_note = models.CharField("Free text note describing extraction method", max_length=255, blank=True)
+    protocol_note = models.TextField("Free text note describing extraction method", blank=True)
 
     def __unicode__(self):
         return "Extract " + self.id + " " + str(self.date)
@@ -139,7 +157,7 @@ class Library(models.Model):
     extract = models.ForeignKey(Extract)
     date = models.DateField("Date library was made")
     protocol_ref = models.CharField("Publication describing library construction method", max_length=255)
-    protocol_note = models.CharField(max_length=255, blank=True)
+    protocol_note = models.TextField("Free text note briefly describing construction method/protocol used", blank=True)
     source = models.CharField("Type of material", choices=LIB_SOURCES, max_length=255)
     layout = models.CharField("Layout/construction method", choices=(('single', 'Single'),('paired','Paired')), max_length=255)
     type = models.CharField("Type of library", choices=(('amplicon','Amplicon'),('shotgun','Shotgun')), max_length=255)
@@ -165,6 +183,9 @@ class Sequence(models.Model):
     qualscale = models.CharField("Scale used for quality read score", max_length=255, default="Phred")
     error_rate = models.PositiveSmallIntegerField("Estimated error rate: 1 x 10^x", blank=True, null=True)
     error_method = models.CharField("Method of calculating estimated error rate", max_length=255, blank=True)
+    demulti_prog = models.CharField("Index demultiplexing program", max_length=255, blank=True)
+    demulti_prog_ver = models.CharField("Index demultiplexing program version", max_length=255, blank=True)
+    demulti_prog_opt = models.CharField("Index demultiplexing program options", max_length=255, blank=True)
 
     def __unicode__(self):
         return "Sequence " + self.id + " " + str(self.date)
@@ -174,8 +195,8 @@ class Processing(models.Model):
     sequence = models.OneToOneField(Sequence)
     analysis = models.ForeignKey('Analysis')
     reference = models.CharField("Reference genome or library to which sequence was aligned", max_length=255)
-    fold_coverage = models.PositiveSmallIntegerField("Average fold coverage of reference sequence", blank=True, null=True)
-    percent_coverage = models.DecimalField("Percentage coverage of reference sequence", max_digits=3, decimal_places=0, blank=True, null=True)
+    fold_coverage = models.DecimalField("Average fold coverage of reference sequence", max_digits=6, decimal_places=3, blank=True, null=True)
+    percent_coverage = models.DecimalField("Percentage coverage of reference sequence", max_digits=6, decimal_places=3, blank=True, null=True)
     contigs = models.PositiveSmallIntegerField("Number of contiguous sequences", blank=True, null=True)
 
     def __unicode__(self):
@@ -186,9 +207,8 @@ class Analysis(models.Model):
         verbose_name_plural = 'analyses'
 
     id = models.CharField("Analysis ID", primary_key=True, max_length=255)
+    dataset = models.OneToOneField('Dataset')
+    note = models.TextField("temp text field")
 
     def __unicode__(self):
         return "Analysis " + self.id
-
-    # more fields to come here
-    #dataset = models.OneToOneField(Dataset)
