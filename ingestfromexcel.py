283ef25b2d546739f5d1a4314c6111b51f0429e9
Christopher Franks <christopher.franks@adelaide.edu.au>
2015-01-12 12:45:06 +1030

    Re #24 now just returns a JSON representation of Excel data

--- ingestfromexcel.py
+++ ingestfromexcel.py
@@ -1,54 +1,52 @@
 import os, argparse
 from xlrd import open_workbook
 import re
+import json
 
-''' This is just a dummy function for testing purposes '''
-def Source(**kwargs):
-    for key, value in kwargs.iteritems():
-        print '%s = %s' % (key, value)
 
-def nomatch(**kwargs):
-    print 'No match for sheet name'
-
-switch = {
-    # To create a Django object would use Source.objects.create
-    'Source': Source,
-}
-
-class IngestFromExcel(object):
-    def __init__(self, input):
-        ''' Create an xlrd Book object with the contents of our Excel input '''
+class ReadFromExcel(object):
+    """
+    Read data from Excel source and return a JSON representation suitable for
+    passing to Django's serializers.deserialize()
+    """
+    def __init__(self, input, prefix='acad'):
+        """ Create an xlrd Book object with the contents of our Excel input """
         self.wb = open_workbook(file_contents=input.read())
 
-    def get_sheetnames(self):
-        for sheet in self.wb.sheets():
-            print sheet.name
+        self.prefix = prefix
 
-    def create_from_sheets(self):
+    def json_from_sheets(self):
+        data = []
+
         for sheet in self.wb.sheets():
-            ''' This dictionary will hold the element names and values read
-            from the sheet '''
             dict = {}
 
-            ''' Skip first row as this has column headers. Assume element name
-            in column 1, value in column 3. '''
+            """ Assume we should skip the first row as it contains column
+            headers, and that element names are in column 1 and values in
+            column 3. """
             for row in range(1, sheet.nrows):
-                dict[str(sheet.cell_value(row,0))] = str(sheet.cell_value(row,2))
+                dict[str(sheet.cell_value(row, 0))] = str(sheet.cell_value(row, 2))
 
-            ''' Each workbook may have multiple sheets of same category named
+            """ Assume each sheet has an element name 'id' which corresponds to
+            pk of model """
+            pk = dict.pop('id')
+
+            """ Each workbook may have multiple sheets of same category named
             Source1, Source2, ... Source11 etc. Strip any number of decimal
-            digits from end of sheet name to get just the category name.
-            '''
+            digits from end of sheet name to get just the category name. """
             category = re.sub('\d*$', '', sheet.name)
+            model = self.prefix + '.' + category.lower()
 
-            ''' Look up our category name and pass dictionary as kwargs '''
-            switch.get(category, nomatch)(**dict)
+            data.append({'fields': dict, 'pk': pk, 'model': model})
+
+        jsondata = json.dumps(data, indent=2)
+        return jsondata
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('infilename', help='Excel source file')
     args = parser.parse_args()
 
-    infile = open(args.infilename, 'rb')
-    excel = IngestFromExcel(infile)
-    excel.create_from_sheets()
+    with open(args.infilename, 'rb') as infile:
+        excel = ReadFromExcel(infile)
+        print excel.json_from_sheets()
